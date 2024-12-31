effe04c891ebb8c4cf3874a8cbf8656b148079e5
wasif12334 <kashifaliraza95@gmail.com>
2024-12-31 07:44:53 +0500

    added the full functionality

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 const inbox = document.getElementById("input-box");
-const ul = document.getElementsByClassName("list-container");
+const ul = document.getElementById("list-container");
 
 function addtask(){
     if (inbox.value.trim() === '') {
         alert("You must write something ");
     }
     else{
+     
         let li = document.createElement('li');
-        li.innerHTML = inbox.value.trim();
+        li.innerHTML = inbox.value;
         ul.appendChild(li);
-        inbox.value = '';
+       let span= document.createElement('span');
+       span.innerHTML = "\u00d7";
+       li.appendChild(span);
+      
     }
+    inbox.value = '';
 }
+ul.addEventListener("click", function(e) {
+    if(e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+    }
+    else if(e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+    }
+}, false);
